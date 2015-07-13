1a1fb7a3214e029404535eb3b3ac7531fa159cca
Huy Pham <phamhu@onid.oregonstate.edu>
2015-07-12 21:45:06 -0700

    Added divide and conquer algorithms as well as displaying run-time for each algorithm. Everything is working!

--- MSS.cpp
+++ MSS.cpp
@@ -1,9 +1,7 @@
 /*
- *      Author: Michael Marven
- *              Huy Pham
- *              Mike Sharkazy
+ *      Author: Mike Sharkazy & Huy Pham
  *   	Date Created: 07/11/15
- *      Last Date Modified: 07/11/15
+ *      Last Date Modified: 07/12/15
  *      File Name: MSS.cpp
  *     	Assignment: Project 1 - Project Group 18
  *      Overview: A program to calculate the maximum subarray sum of arrays
@@ -62,6 +60,23 @@ void enumMSS(const std::vector<int> &a, std::vector<int> &sa, int &max);
 void betterEnumMSS(const std::vector<int> &a, std::vector<int> &sa, int &max);
 
 /*
+ * The dncMSS() function calculates the maximum sum of a subarray of an array
+ * using divide and conquer algorithm
+ */
+void dncMSS(const std::vector<int> &a, std::vector<int> &sa, int &max);
+
+/*
+ * Recursive function for the divide and conquer algorithm
+ */
+int dncMSS_recursive(const std::vector<int> &a, int left, int right, int &startIndex, int &endIndex);
+
+/*
+ * Linear function calculate the maximum sum of a subarray of a given array
+ * using linear algorithm
+ */
+void linearMSS(const std::vector<int> &a, std::vector<int> &sa, int &max);
+
+/*
  * The outputToFile() function writes the vectors and sum to the output file
  */
 void outputToFile(std::ofstream &out, const std::vector<int> &a,
@@ -97,25 +112,63 @@ int main(int argc, char *argv[])
     	// Declare subarray vectors and sum variable
     		std::vector<int> subArray1;
     		std::vector<int> subArray2;
+		std::vector<int> subArray3;
+		std::vector<int> subArray4;
     		int sum = 0;
    	 	if(line.size() > 0) {
-    	// Call function to calculate the maximum sum of a subarray
+   	
+	/// Using enumeration method ///	
+        		std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();  //set first time point
+	// Call function to calculate the maximum sum of a subarray
     			enumMSS(intVect, subArray1, sum);
    	 
-    	// Write vector contents and sum to the output file
+   			std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();  //set second time point
+        		auto duration1 = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();      //calculate duration
+
+        		std::cout << "Algorithm 1 took " << duration1 << " milliseconds to run." << std::endl;	
+	// Write vector contents and sum to the output file
     			outputToFile(outStream, intVect, subArray1, sum);
-   	 
-    	// Call the next function to calculate the maximum sum of a subarray
+    
+	/// Using better enumeration method ///
+        		std::chrono::high_resolution_clock::time_point t3 = std::chrono::high_resolution_clock::now();  //set first time point
+	// Call the next function to calculate the maximum sum of a subarray
     			betterEnumMSS(intVect, subArray2, sum);
-   	
+    			std::chrono::high_resolution_clock::time_point t4 = std::chrono::high_resolution_clock::now();  //set second time point
+        		auto duration2 = std::chrono::duration_cast<std::chrono::microseconds>( t4 - t3 ).count();      //calculate duration
+
+        		std::cout << "Algorithm 2 took " << duration2 << " milliseconds to run." << std::endl;	  	
     	// Write vector contents and sum to the output file
     			outputToFile(outStream, intVect, subArray2, sum); 	 
+		
+	/// Using divide and conquer method ///
+			std::chrono::high_resolution_clock::time_point t5 = std::chrono::high_resolution_clock::now();  //set first time point
+	// Call the next function to calculate the max sum
+			dncMSS(intVect, subArray3, sum);
+    			std::chrono::high_resolution_clock::time_point t6 = std::chrono::high_resolution_clock::now();  //set second time point
+        		auto duration3 = std::chrono::duration_cast<std::chrono::microseconds>( t6 - t5 ).count();      //calculate duration
+         		std::cout << "Algorithm 3 took " << duration3 << " milliseconds to run." << std::endl;	  	
+    	// Write vector contents and sum to the output file
+    			outputToFile(outStream, intVect, subArray3, sum); 	 
+
+	/// Using linear method ///
+			std::chrono::high_resolution_clock::time_point t7 = std::chrono::high_resolution_clock::now();  //set first time point
+	// Call the next function to calculate the max sum
+			linearMSS(intVect, subArray4, sum);
+    			std::chrono::high_resolution_clock::time_point t8 = std::chrono::high_resolution_clock::now();  //set second time point
+        		auto duration4 = std::chrono::duration_cast<std::chrono::microseconds>( t8 - t7 ).count();      //calculate duration
+         		std::cout << "Algorithm 4 took " << duration4 << " milliseconds to run." << std::endl;	  	
+    	// Write vector contents and sum to the output file
+    			outputToFile(outStream, intVect, subArray4, sum); 	 
+
+
+
+
 		}
     	}
 	// Close input and output streams
 	inStream.close();
 	outStream.close();
- 	std::cout << "Program executed successfully!\n"; 
+ 	std::cout << "Main program executed successfully!\n"; 
 }
 
 /*   *   *   *   *   *   *
@@ -213,6 +266,154 @@ void betterEnumMSS(const std::vector<int> &a, std::vector<int> &sa, int &max)
 	}
 }
 
+/*   *   *   *   *   *   *
+ *
+ * Function: dncMSS()
+ *
+ *	Entry: A const vector by reference, a vector by reference for the
+ *       	subarray, and an int by reference
+ *
+ * 	Exit: Values of parameters sa and sum will be changed by the function        	 
+ *
+ *  Purpose: Calculate the maximum sum of a subarray using divide and conquer 
+ *
+ *   *   *   *   *   *   */
+void dncMSS(const std::vector<int> &a, std::vector<int> &sa, int &max)
+{
+	//std::cout << "In main dnc\n";
+
+	int beg = 0;
+	int end = a.size() - 1;
+	int startIndex = 0;
+	int endIndex = 0;
+	
+	max = dncMSS_recursive(a, beg, end, startIndex, endIndex);
+
+	// Create the subarray
+	for (int i = startIndex; i <= endIndex; i++)
+	{
+		sa.push_back(a[i]);
+	}
+}
+
+int dncMSS_recursive(const std::vector<int> &a, int left, int right, int &startIndex, int &endIndex)
+{
+	//std::cout << "In recursive dnc \n";
+	//Base case
+	if(left == right)
+	{
+		startIndex = left;
+		endIndex = right;
+		return a[left];
+	}
+	int l_idx_start, l_idx_end, r_idx_start, r_idx_end, m_idx_start, m_idx_end;
+	int max = 0;
+	int mid = (left + right) / 2;
+    	int maxLeft = dncMSS_recursive(a, left, mid, l_idx_start, l_idx_end);
+	int maxRight = dncMSS_recursive(a, mid + 1, right, r_idx_start, r_idx_end);
+	int maxMidLeft = 0, maxMidRight = 0, leftSum = 0, rightSum = 0;
+
+	// Calculate maximum subarray of mid left side
+	for (int i = mid; i >= left; i--)
+	{
+		leftSum += a[i];
+		if(leftSum > maxMidLeft)
+		{
+			maxMidLeft = leftSum;
+			m_idx_start = i;
+		}	
+	}
+    	// Calculate maximum subarray of mid right side
+    	for (int i = mid + 1; i <= right; i++)
+	{
+		rightSum += a[i];
+		if(rightSum > maxMidRight)
+		{
+			maxMidRight = rightSum;
+			m_idx_end = i;
+		}
+	}
+	
+	if(maxLeft > maxRight)
+	{
+		if(maxLeft > maxMidLeft + maxMidRight)
+		{
+			startIndex = l_idx_start;
+			endIndex = l_idx_end;
+			return maxLeft;
+		}
+		else
+		{
+			startIndex = m_idx_start;
+			endIndex = m_idx_end;
+			return maxMidLeft + maxMidRight;
+		}
+	}
+	else 
+	{
+		if(maxRight > maxMidLeft + maxMidRight)
+		{
+			startIndex = r_idx_start;
+			endIndex = r_idx_end;
+			return maxRight;
+		}
+		else 
+		{
+			startIndex = m_idx_start;
+			endIndex = m_idx_end;
+			return maxMidLeft + maxMidRight;
+		}
+	}
+}
+
+/*   *   *   *   *   *   *
+ *
+ * Function: linearMSS()
+ *
+ *	Entry: A const vector by reference, a vector by reference for the
+ *       	subarray, and an int by reference
+ *
+ * 	Exit: Values of parameters sa and sum will be changed by the function        	 
+ *
+ *  Purpose: Calculate the maximum sum of a subarray using linear algorithm
+ *
+ *   *   *   *   *   *   */
+void linearMSS(const std::vector<int> &a, std::vector<int> &sa, int &max)
+{
+	//std::cout << "In linear\n";
+	int startIndex = 0;
+	int endIndex = 0;
+	int currentMax = 0;
+	max = 0;
+	int i_low = 0;
+	int i_high = 0;;
+
+	for (int i = 1; i < a.size() ; i++)
+	{
+		i_high = i;
+		if(currentMax > 0)
+		{
+			currentMax += a.at(i); 
+		}
+		else
+		{
+			i_low = i;
+			currentMax = a.at(i);
+		}
+
+		if(currentMax > max)
+		{
+			max = currentMax;
+			startIndex = i_low;
+			endIndex = i_high;
+		}
+	}
+	// Create the subarray
+	for (int i = startIndex; i <= endIndex; i++)
+	{
+		sa.push_back(a[i]);
+	}
+}
 /*   *   *   *   *   *   *
  *
  * Function: outputToFile()
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # CS325_Max_Sum_Subarray
 This program will calculate the maximum sum of a sub-array for a given integer array. The program will implement four different algorithms.
+
+Once the program is compiled, the code can be executed against an input file that has one or many integer arrays. The program will execute four algorithms (enumeration, better enumeration, divide and conquer and linear) on  each array and determine the maximum sum of the sub array. The result is written to output file 'MSS_Results.txt'.
+
+For testing purposes, different input files are provided such as 'test1.txt', 'MSS_100.txt' - 'MSS_1000.txt'. 'test1.txt' consists of a small set of arrays. 'MSS_100.txt' to 'MSS_1000.txt' consists of 10 arrays in each file where each array has 100 to 1000 random integers.
+
+Example:
+1) Compile the program - 'g++ -std=c++0x MSS.cpp -o mss'
+2) Execute the program against 'test1.txt' file - 'mss test1.txt'
+3) Run-time for each algorithm will be displayed on the screen and the results will be stored under 'MSS_Results.txt'.
